64722f5c0127efb27f32850cda3659427133b3de
Matt McFarland <contact@mattmcfarland.com>
2017-04-10 06:42:07 -0400

    code cleanup

--- scratch.js
+++ scratch.js
@@ -1,7 +1,6 @@
 const Chance = require('chance')
 const range = require('fill-range')
 const plotLine = require('bresenham-js')
-const map = []
 const config = {
   seed: undefined,
   mapWidth: 150,
@@ -11,52 +10,19 @@ const config = {
   maxRoomWidth: 23,
   maxRoomHeight: 12
 }
-
 const chance = Chance(config.seed)
-const toInt = (str) => parseInt(str)
-const getMapCapacity = (mapWidth, mapHeight, maxRoomWidth, maxRoomHeight) =>
-  Math.floor((mapWidth / maxRoomWidth) * (mapHeight / maxRoomHeight))
 
-const createHashGrid = (columnLength, rowLength, value) => {
-  const result = {}
-  for (let col = 0; col < columnLength; col++) {
-    for (let row = 0; row < rowLength; row++) {
-      result[`${col}:${row}`] = typeof value === 'function' ? value(col, row) : value
-    }
-  }
-  return result
+const myCoolMap = initMap(config)
+const rooms = createRooms(myCoolMap, config)
+connectRooms(myCoolMap, rooms)
+createCooridoors(myCoolMap, rooms)
+printMap(myCoolMap)
+
+function initMap({mapWidth, mapHeight}) {
+  return fillRect([], 1, 0, 0, mapWidth, mapHeight)
 }
 
-const transposeArray = (array) => array.map((col, i) =>
-  array.reduce(function (acc, row) {
-    if (row[i] >= 0)
-    acc.push(row[i])
-    return acc
-  }, [])
-);
-
-function fillRect(area, value, x, y, x2, y2) {
-  for (let _x = x; _x < x2; _x++) {
-    for (let _y = y; _y < y2; _y++) {
-      area[_x] || (area[_x] = [])
-      area[_x][_y] = value
-    }
-  }
-}
-
-function digRect(area, value, x, y, x2, y2) {
-  for (let _x = x; _x < x2; _x++) {
-    for (let _y = y; _y < y2; _y++) {
-      area[_x] || (area[_x] = [])
-      const isCorner = (_x === x || _x === x2 - 1) && (_y === y || _y === y2 - 1)
-      const isEdge = !isCorner && (_x === x || _x === x2 - 1) || (_y === y || _y === y2 - 1)
-      const previousIsWall = area[_x - 1][_y] === 1
-      area[_x][_y] = isCorner ? chance.pickone([0, 1]) : previousIsWall && isEdge ? chance.pickone([0, 1]) : value
-    }
-  }
-}
-
-function createRooms(options) {
+function createRooms(area, options) {
   const {
     mapWidth,
     mapHeight,
@@ -79,13 +45,13 @@ function createRooms(options) {
     const x2 = mapX + width - 2
     const y2 = mapY + height - 2
     const rect = [mapX, mapY, x2, y2]
-    digRect(map, 0, mapX, mapY, x2, y2)
+    digRect(area, 0, mapX, mapY, x2, y2)
     return { width, height, mapX, mapY, rect, connections: [] }
   })
   return grid
 }
 
-function createCooridoors(rooms) {
+function createCooridoors(area, rooms) {
   Object.keys(rooms).forEach((roomKey) => {
     const room = rooms[roomKey]
     room.connections.forEach(otherRoomKey => {
@@ -105,16 +71,38 @@ function createCooridoors(rooms) {
       let points = plotLine([pointA.x, pointA.y], [pointB.x, pointB.y])
       points.forEach(([px, py]) => {
         let widen = chance.pickone([-1, 1, 0])
-        if (map[px + widen]) map[px + widen][py] = 0
+        if (area[px + widen]) area[px + widen][py] = 0
         widen = chance.pickone([-1, 1])
-        if (map[px + widen]) map[px + widen][py] = 0
-        map[px][py] = 0
+        if (area[px + widen]) area[px + widen][py] = 0
+        area[px][py] = 0
       })
     })
   })
 }
 
-function connectRooms(rooms) {
+function fillRect(area, value, x, y, x2, y2) {
+  for (let _x = x; _x < x2; _x++) {
+    for (let _y = y; _y < y2; _y++) {
+      area[_x] || (area[_x] = [])
+      area[_x][_y] = value
+    }
+  }
+  return area
+}
+
+function digRect(area, value, x, y, x2, y2) {
+  for (let _x = x; _x < x2; _x++) {
+    for (let _y = y; _y < y2; _y++) {
+      area[_x] || (area[_x] = [])
+      const isCorner = (_x === x || _x === x2 - 1) && (_y === y || _y === y2 - 1)
+      const isEdge = !isCorner && (_x === x || _x === x2 - 1) || (_y === y || _y === y2 - 1)
+      const previousIsWall = area[_x - 1][_y] === 1
+      area[_x][_y] = isCorner ? chance.pickone([0, 1]) : previousIsWall && isEdge ? chance.pickone([0, 1]) : value
+    }
+  }
+}
+
+function connectRooms(area, rooms) {
   // pick one room, find a neighbor, then pick the next one, then find another neighbor, then pick another one, then find another neighbor, until this is exhausted.
   const UP = [0, -1]
   const DOWN = [0, 1]
@@ -140,10 +128,12 @@ function connectRooms(rooms) {
     directions.shift()
   } while (directions.length)
   let unConnectedRooms = Object.keys(rooms).filter((roomKey) => rooms[roomKey].connections.length === 0)
+  directions = chance.shuffle([UP, DOWN, LEFT, RIGHT])
   do {
+    if (directions.length === 0) break;
     unConnectedRooms = Object.keys(rooms).filter((roomKey) => rooms[roomKey].connections.length === 0)
     unConnectedRooms.forEach(roomKey => {
-      let directions = chance.shuffle([UP, DOWN, LEFT, RIGHT])
+      directions = chance.shuffle([UP, DOWN, LEFT, RIGHT])
       do {
         let targetCoords = getRelativeRoomKey(roomKey, chance.pickone(directions))
         if (rooms[targetCoords]
@@ -160,10 +150,32 @@ function connectRooms(rooms) {
   //rooms
 }
 
-fillRect(map, 1, 0, 0, config.mapWidth, config.mapHeight)
-const rooms = createRooms(config)
-connectRooms(rooms)
-createCooridoors(rooms)
+function toInt (str) { return parseInt(str) }
+
+function getMapCapacity(mapWidth, mapHeight, maxRoomWidth, maxRoomHeight) {
+  return Math.floor((mapWidth / maxRoomWidth) * (mapHeight / maxRoomHeight))
+}
+
+function createHashGrid (columnLength, rowLength, value) {
+  const result = {}
+  for (let col = 0; col < columnLength; col++) {
+    for (let row = 0; row < rowLength; row++) {
+      result[`${col}:${row}`] = typeof value === 'function' ? value(col, row) : value
+    }
+  }
+  return result
+}
+
+function transposeArray(array) {
+  return array.map((col, i) =>
+    array.reduce(function (acc, row) {
+      if (row[i] >= 0)
+        acc.push(row[i])
+      return acc
+    }, [])
+  );
+}
+
 function printMap(map) {
   str = ''
   transposeArray(map).forEach((row, y) => {
